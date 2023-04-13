6e009a6fb1785b69c97a27a03b6f3b8523d011ba
Lev <lev.yakovenko11@gmail.com>
2023-04-13 17:34:13 +0700

    changed writer: check_input_data

--- parser_logfile/gui_entry_point.py
+++ parser_logfile/gui_entry_point.py
@@ -7,6 +7,7 @@ import os
 
 
 class Root(Tk):
+    """ Класс окна программы"""
     def __init__(self, *args, **kwargs):
         Tk.__init__(self, *args, **kwargs)
         self.input_file_entry = Entry()
